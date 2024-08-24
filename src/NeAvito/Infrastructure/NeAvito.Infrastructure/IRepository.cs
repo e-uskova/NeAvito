a4eb64392beedc3a70d9adff1ddeb05f7daaf3fd
Ekaterina <60484474+e-uskova@users.noreply.github.com>
2024-08-24 10:11:32 +0300

    Init base repository

---
+++ src/NeAvito/Infrastructure/NeAvito.Infrastructure/IRepository.cs
@@ -0,0 +1,39 @@
+﻿namespace NeAvito.Infrastructure
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Получение всех элементов сущности <see cref="TEntity"/>
+        /// </summary>
+        /// <returns>Все элементы сущности <see cref="TEntity"/></returns>
+        IEnumerable<TEntity> GetAll();
+
+        /// <summary>
+        /// Получение элементов сущности <see cref="TEntity"/> по условию
+        /// </summary>
+        /// <param name="predicate">Условие отбора</param>
+        /// <returns>Элементы сущности <see cref="TEntity"/>, соответвующие условию отбора</returns>
+        IEnumerable<TEntity> GetByPredicate(Func<TEntity, bool> predicate);
+
+        /// <summary>
+        /// Создание сущности <see cref="TEntity"/>
+        /// </summary>
+        /// <param name="entity">Модель сущности</param>
+        /// <returns>Созданная сущность</returns>
+        TEntity Add(TEntity entity);
+
+        /// <summary>
+        /// Изменение сущности <see cref="TEntity"/>
+        /// </summary>
+        /// <param name="entity">Модель сущности</param>
+        /// <returns>Измененная сущность</returns>
+        TEntity Update(TEntity entity);
+        
+        /// <summary>
+        /// Удаление сущности 
+        /// </summary>
+        /// <param name="id">Идентификатор сущности</param>
+        /// <returns></returns>
+        bool Delete(Guid id);
+    }
+}
---
+++ src/NeAvito/Infrastructure/NeAvito.Infrastructure/Repository.cs
@@ -0,0 +1,31 @@
+﻿namespace NeAvito.Infrastructure
+{
+    /// <inheritdoc />
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetByPredicate(Func<TEntity, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
