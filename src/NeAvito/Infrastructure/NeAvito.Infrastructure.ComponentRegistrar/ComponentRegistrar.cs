4c65920fa97d379c75c1d66ee03fe3206a08af73
Ekaterina <60484474+e-uskova@users.noreply.github.com>
2024-08-24 10:11:00 +0300

    Init ComponentRegistrar

---
+++ src/NeAvito/Infrastructure/NeAvito.Infrastructure.ComponentRegistrar/ComponentRegistrar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NeAvito.Infrastructure.ComponentRegistrar
+{
+    public static class ComponentRegistrar
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //TODO
+            return services;
+        }
+    }
+}
